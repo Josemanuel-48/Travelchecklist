01896dd80e6aa1b1c029886d77b7336ebbc32b49
josemanuel-48 <josefronteddev@gmail.com>
2026-06-10 16:06:29 +0200

    creada app

---
+++ trips/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path  # funcion para definir rutas
+from . import views            # importamos las vistas de esta misma app
+
+app_name  = "trips"           # espacio de nombres para usar rutas como trips:home
+
+urlpatterns = [
+    path('', views.home, nama='home'), # ruta raiz de la app: llama a la vista home
+]
--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request): # crear funcion
+    # renderiza(cargando en pantalla) la plantilla de inicio y la devuelve como respuesta HTTP
+    return render(request, 'trips/home.html')
