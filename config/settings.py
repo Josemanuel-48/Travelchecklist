cff8af404f6ec81b7bbe67d91c4f1d682d39af82
josemanuel-48 <josefronteddev@gmail.com>
2026-06-10 15:41:48 +0200

    creada app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trips', # nuestra app principal creada por mi.
 ]
 
 MIDDLEWARE = [
