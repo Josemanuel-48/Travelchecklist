f8d7dcbff2195b329ee27a5963444ab83f5e6902
josemanuel-48 <josefronteddev@gmail.com>
2026-06-10 19:26:37 +0200

    creada html

--- trips/urls.py
+++ trips/urls.py
@@ -4,5 +4,5 @@ from . import views            # importamos las vistas de esta misma app
 app_name  = "trips"           # espacio de nombres para usar rutas como trips:home
 
 urlpatterns = [
-    path('', views.home, nama='home'), # ruta raiz de la app: llama a la vista home
+    path('', views.home, name='home'), # ruta raiz de la app: llama a la vista home
 ]
