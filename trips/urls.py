d6b9eecfa1888fb0a4f95dd6fbc1678e7f85a244
josemanuel-48 <josefronteddev@gmail.com>
2026-06-17 16:15:50 +0200

    forms

--- trips/urls.py
+++ trips/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path  # funcion para definir rutas
 from . import views            # importamos las vistas de esta misma app
 
@@ -5,4 +6,11 @@ app_name  = "trips"           # espacio de nombres para usar rutas como trips:ho
 
 urlpatterns = [
     path('', views.home, name='home'), # ruta raiz de la app: llama a la vista home
+    
+    path('signup/', views.signup, name='signup'), # ruta para el registro de usuarios.
+
+    # login usando la vista generica de Django y nuestro template personalizado.
+    path('login/', auth_views.LoginView.as_view(template_name='trips/login.html'), name='login'),
+
+    # path('logout/', views.logout, name='logout'), # ruta para el logout de usuarios
 ]
--- trips/views.py
+++ trips/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def home(request): # crear funcion
     # renderiza(cargando en pantalla) la plantilla de inicio y la devuelve como respuesta HTTP
     return render(request, 'trips/home.html')
+
+def signup(request):
+    # si el usuario envia el formulario procesamos datos.
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        # si el formulario es valido, guardamos el nuevo usuario.
+        if form.is_valid():
+            form.save()
+            return redirect('trips:login') # tras registrarse, lo envia,os al login
+        
+    else:
+        # si entra pot GET; mostramos el formulario vacio
+        form = UserCreationForm()
+
+        # renderizamos el template pasando el formulario al contexto
+        return render(request, 'trips/signup.html', {'form': form})
